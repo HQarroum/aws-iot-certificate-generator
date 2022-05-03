47888626f9e4b355e7c8a94fc48ba723084119bb
Halim Qarroum <hqm.post@gmail.com>
2022-05-03 13:42:01 +0200

    Code lint

--- bin/connect-device.sh
+++ bin/connect-device.sh
@@ -10,29 +10,48 @@ ROOT_CERT_URL='https://www.amazontrust.com/repository/AmazonRootCA1.pem'
 ROOT_CERT_NAME='aws-root-cert.pem'
 ROOT_CERT_PATH="$OUTPUT_DIRECTORY/$ROOT_CERT_NAME"
 DEVICE_CERTS_DIRECTORY="$DIR/device-certs"
-CERTIFICATE_NAME='device-certificate'
-THING_NAME='thing-1234'
-PORT=8883
+CERTIFICATE_NAME="device-certificate"
+THING_NAME="thing-1234"
 AWS_IOT_ENDPOINT=$(aws iot describe-endpoint --endpoint-type iot:Data-ATS --query endpointAddress --output text)
+
+# The help usage text.
+USAGE="Initiates an MQTTS connection to AWS IoT using the generated device certificates.
+Options :
+    -t  (Optional) - the thing name to use
+    -d  (Optional) - the path of the directory containing the device certificate"
+
+# Retrieving arguments from the command-line.
+while getopts ":t:d:h" o; do
+  case "${o}" in
+    t) THING_NAME=${OPTARG} ;;
+    d) DEVICE_CERTS_DIRECTORY=${OPTARG} ;;
+    h) echo "$USAGE"
+       exit 0 ;;
+   \?) echo "Invalid option: -$OPTARG" >&2
+       exit 1 ;;
+    :) echo "Option -$OPTARG requires an argument." >&2
+       exit 1 ;;
+  esac
+done
 
 # If it doesn't already exist, we download the AWS root certificate required
 # to create a connection to AWS IoT, and for the local TLS agent to acknowledge
 # that the remote platform is indeed AWS IoT, and not a man in the middle.
 if [ ! -f "$ROOT_CERT_PATH" ]; then
   echo "[+] AWS Root Certificate not detected, downloading it ..."
-  curl $ROOT_CERT_URL | tee "$ROOT_CERT_PATH"
+  curl "$ROOT_CERT_URL" | tee "$ROOT_CERT_PATH"
   echo "[+] The AWS Root certificate has been successfully saved in the local directory ($ROOT_CERT_PATH)"
 fi
 
 # Connecting to AWS IoT using the generated certificates
 # and publishing a message.
 mosquitto_pub -d \
-  --cafile aws-root-cert.pem \
+  --cafile "$ROOT_CERT_NAME" \
   --cert "$DEVICE_CERTS_DIRECTORY/$CERTIFICATE_NAME.crt" \
   --key "$DEVICE_CERTS_DIRECTORY/$CERTIFICATE_NAME.key" \
-  -h $AWS_IOT_ENDPOINT \
-  -p $PORT \
-  -t $THING_NAME/telemetry \
-  -i $THING_NAME \
+  -h "$AWS_IOT_ENDPOINT" \
+  -p 8883 \
+  -t "$THING_NAME/telemetry" \
+  -i "$THING_NAME" \
   --tls-version tlsv1.2 \
   -m "{ \"message\": \"Hello World !\" }"
--- bin/create-device-certificate.sh
+++ bin/create-device-certificate.sh
@@ -14,15 +14,15 @@ CERTIFICATE_NAME='device-certificate'
 # The help usage text.
 USAGE="Generates device certificates compatible with the AWS IoT Just-In-Time-Registration process given Root CA certificates.
 Options :
-    -o  (Optional) - the path to the OpenSSL config file to use
-    -d  (Optional) - the path of the directory containing the Root CA certificates
-    -r  (Optional) - the location in which the AWS IoT Root CA will be downloaded"
+    -c  (Optional) - the path to the OpenSSL config file to use
+    -d  (Optional) - the path of the directory containing the CA certificates
+    -o  (Optional) - the output directory to write the certificates to"
 
 # Retrieving arguments from the command-line.
-while getopts ":d:r:o:h" o; do
+while getopts ":d:c:o:h" o; do
   case "${o}" in
-    r) ROOT_CERT_NAME=${OPTARG} ;;
-    o) OPENSSL_CONFIG=${OPTARG} ;;
+    o) OUTPUT_DIRECTORY=${OPTARG} ;;
+    c) OPENSSL_CONFIG=${OPTARG} ;;
     d) ROOT_CA_DIRECTORY=${OPTARG} ;;
     h) echo "$USAGE"
        exit 0 ;;
@@ -34,42 +34,42 @@ while getopts ":d:r:o:h" o; do
 done
 
 # Verifying that the Root CA directory exists.
-if [ ! -d $ROOT_CA_DIRECTORY ]; then
+if [ ! -d "$ROOT_CA_DIRECTORY" ]; then
   echo "[!] The Root CA directory ($ROOT_CA_DIRECTORY) does not exist. Did you execute the 'create-and-register-ca.sh' script first to generate a CA ?"
   exit 1
 fi
 
 # Verifying that the Root CA directory contains certificates.
-if [ ! -f $ROOT_CA_DIRECTORY/$CACERT_NAME.pem ] || [ ! -f $ROOT_CA_DIRECTORY/$CACERT_NAME.key ]; then
+if [ ! -f "$ROOT_CA_DIRECTORY/$CACERT_NAME.pem" ] || [ ! -f "$ROOT_CA_DIRECTORY/$CACERT_NAME.key" ]; then
   echo "[!] The Root CA directory ($ROOT_CA_DIRECTORY) does not contain valid Root CA certificates (expected '$ROOT_CA_DIRECTORY/$CACERT_NAME.pem' and '$ROOT_CA_DIRECTORY/$CACERT_NAME.key' to exist)."
   exit 1
 fi
 
 # Creating the output directory.
-mkdir -p $OUTPUT_DIRECTORY
+mkdir -p "$OUTPUT_DIRECTORY"
 
 # Creates a new X.509 private and public key
 # signed by the generated CA and stores them
 # in the output directory.
 function create_device_certificate() {
-  openssl genrsa -out $OUTPUT_DIRECTORY/$CERTIFICATE_NAME.key 2048
+  openssl genrsa -out "$OUTPUT_DIRECTORY/$CERTIFICATE_NAME.key" 2048
 
   # Creating a CSR.
   openssl req \
-    -config $OPENSSL_CONFIG \
+    -config "$OPENSSL_CONFIG" \
     -new \
-    -key $OUTPUT_DIRECTORY/$CERTIFICATE_NAME.key \
-    -out $OUTPUT_DIRECTORY/$CERTIFICATE_NAME.csr
+    -key "$OUTPUT_DIRECTORY/$CERTIFICATE_NAME.key" \
+    -out "$OUTPUT_DIRECTORY/$CERTIFICATE_NAME.csr"
 
   # Creating the device certificate using the given Certificate Authority.
   openssl x509 \
     -req \
-    -in $OUTPUT_DIRECTORY/$CERTIFICATE_NAME.csr \
-    -CA $ROOT_CA_DIRECTORY/$CACERT_NAME.pem \
-    -CAkey $ROOT_CA_DIRECTORY/$CACERT_NAME.key \
+    -in "$OUTPUT_DIRECTORY/$CERTIFICATE_NAME.csr" \
+    -CA "$ROOT_CA_DIRECTORY/$CACERT_NAME.pem" \
+    -CAkey "$ROOT_CA_DIRECTORY/$CACERT_NAME.key" \
     -CAcreateserial \
-    -CAserial $OUTPUT_DIRECTORY/$CERTIFICATE_NAME.srl \
-    -out $OUTPUT_DIRECTORY/$CERTIFICATE_NAME.crt \
+    -CAserial "$OUTPUT_DIRECTORY/$CERTIFICATE_NAME.srl" \
+    -out "$OUTPUT_DIRECTORY/$CERTIFICATE_NAME.crt" \
     -days 365 \
     -sha256
 
